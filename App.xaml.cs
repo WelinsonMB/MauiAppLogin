e464b6b57f58808055d98f8c8dfa08adadfe4494
WelinsonMB <welinson.mendes2015@hotmail.com>
2024-11-11 10:33:34 -0300

    Adição

--- App.xaml.cs
+++ App.xaml.cs
@@ -7,8 +7,21 @@ namespace MauiAppLogin
         public App()
         {
             InitializeComponent();
+            string? usuario_logado = null;
+            Task.Run(async () =>
+            {
+                usuario_logado = await SecureStorage.Default.GetAsync("usuario_logado");
+                if (usuario_logado == null)
+                {
+                    MainPage = new Login();
+                }
+                else
+                { 
+                    MainPage = new Protegida();
+                }
+            });
 
-            MainPage = new AppShell();
+            MainPage = new Login(); 
         }
 
         protected override Window CreateWindow(IActivationState ActivationState)
---
+++ DadosUsuario.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiAppLogin
+{
+    public class DadosUsuario
+    {
+        public String Usuario {  get; set; }
+        public String Senha { get; set; }
+
+    }
+}
